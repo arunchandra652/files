af38c7688be6e456784ecca5b979ec878065431c
arunchandra652 <arunchandra652@gmail.com>
2023-02-10 11:13:59 +0530

    intial commit

---
+++ EmployeeDetailss/src/org/empp/Employee.java
@@ -0,0 +1,16 @@
+package org.empp;
+
+public class Employee {
+	private void empId(int id) {
+		System.out.println("emp id is "+id);
+	}
+		
+		public static void main(String[]args) {
+			Employee e=new Employee();
+			e.empId(357);
+			e.empId(159);
+
+
+	}
+
+}
